8503d6cd3f9551e9f5a28798d7cf9e215343a858
sgalinsk <sgalinsk@735d13b6-9817-0410-8766-e36946ffe9aa>
2013-05-31 17:30:52 +0000

    [TASK] Usage of Sass
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/df_tools/trunk@75813 735d13b6-9817-0410-8766-e36946ffe9aa

---
+++ config.rb
@@ -0,0 +1,11 @@
+output_style = :expanded
+relative_assets = true
+line_comments = false
+asset_cache_buster :none
+environment = :production
+
+javascripts_dir = 'Scripts'
+css_dir = 'StyleSheets'
+sass_dir = 'Sass'
+images_dir = 'Icons'
+cache_path = '/tmp/sass-cache/';
